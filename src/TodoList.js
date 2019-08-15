e98c20e63f634e69bde6f918aad10a0716d47260
王宁赛 <wangningsai@126.com>
2019-08-15 16:41:21 +0800

    react 项目的简单搭建

---
+++ src/TodoList.js
@@ -0,0 +1,70 @@
+import React, { Component,Fragment } from 'react'
+
+class TodoList extends Component {
+
+	constructor(props) {
+
+		super(props);
+		this.state = {
+			inputValue : '',
+			list:['学习英文','学习React']
+		}
+	}
+
+	render() {
+		return (
+			<Fragment>	
+				<div> 
+					<input 
+						value = {this.state.inputValue}
+						onChange = {this.handleInputChange.bind(this)}
+					/> 
+					<button onClick = {this.handleBtnClick.bind(this)}> 提交 </button>
+				</div>
+
+				<ul>
+					{
+						this.state.list.map((item, index) => {
+
+							return <li 
+								key = {index}
+							  onClick = {this.handleClickDelete.bind(this, index)}>
+							{item}
+							</li>
+						})
+					}
+				</ul>
+
+			</Fragment>
+			)
+	}
+
+	handleInputChange(e) {
+
+		this.setState({
+			inputValue: e.target.value
+		})
+	}
+
+	handleBtnClick() {
+		this.setState({
+			list:[...this.state.list,this.state.inputValue],
+			inputValue:''
+		})
+	}
+
+	handleClickDelete(index) {
+
+		const list = [...this.state.list];
+		list.splice(index,1)
+
+		this.setState({
+			list:list
+		})
+		
+
+	}
+
+}
+
+export default TodoList;
